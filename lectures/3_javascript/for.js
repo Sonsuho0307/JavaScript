32b0a93a38cfdfdb2dd5462e2f65cd020df84d80
Sonsuho0307 <sksh237@naver.com>
2025-06-20 16:20:50 +0900

    6/20

---
+++ lectures/3_javascript/for.js
@@ -0,0 +1,11 @@
+'use strict';
+
+for (let i = 1; i < 5; i++) {
+  console.log(i);
+}
+
+let fruits = ['사과', '바나나', '수박', '파인애플'];
+
+for (let j = 0; j < fruits.length; j++) {
+  console.log(fruits[j]);
+}
---
+++ lectures/3_javascript/while.js
@@ -0,0 +1,13 @@
+'use strict';
+
+let count = 0;
+while (count < 5) {
+  count++;
+  console.log(count);
+}
+
+let number = 0;
+do {
+  number++;
+  console.log(number);
+} while (number < 5);
