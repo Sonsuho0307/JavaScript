edeacb6113fe7f0784efe372870d4a4fba77c1d3
Sonsuho0307 <sksh237@naver.com>
2025-06-25 13:29:28 +0900

    6/25

---
+++ lectures/3_javascript/function.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// 함수 선언식
+
+// function sayHello() {
+//   console.log('이름과 나이를 출력해주세요');
+//   console.log('이름: 손수호, 나이: 25살');
+// }
+
+// 함수 표현식
+const sayHello = function () {
+  console.log('이름과 나이를 출력해주세요');
+  console.log('이름: 손수호, 나이: 25살');
+};
+
+sayHello(); // 함수 호출
