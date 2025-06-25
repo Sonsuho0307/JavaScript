94a4444d96c9a881dc89cdcafc93fcc1c6a93532
Sonsuho0307 <sksh237@naver.com>
2025-06-25 10:24:54 +0900

    6/25

---
+++ lectures/3_javascript/operator.js
@@ -0,0 +1,28 @@
+// 문자열 병합
+console.log('Hello' + 'world');
+console.log('3' + 3); // number -> string
+console.log(typeof ('3' + 3));
+console.log('3' + 5 + 8); // 358
+console.log(3 + 5 + '8'); // 88
+
+//
+
+let x = 10;
+let y = 20;
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x / y);
+// console.log(9 % 2); // 9 에서 2 를 나누고 남은 값 = 1
+
+// let number = prompt('숫자를 입력하세요');
+// if (number % 2 === 1) {
+//   alert('홀수입니다');
+// } else {
+//   alert('짝수네요');
+// }
+
+let result = x + y;
+console.log('x: ', x);
+console.log('y: ', y);
+console.log('result: ', result);
---
+++ lectures/quiz_01/star.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// window.onload = function () {
+//   for (let i = 0; i < 5; i++) {
+//     // for i = 세로(행)
+//     for (let j = 0; j < 5; j++) {
+//       // for j = 가로(열)
+//       document.write('*');
+//     }
+//     document.write('</br>');
+//   }
+// };
+
+window.onload = function () {
+  for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < 5; J++) {
+      document.write('*');
+    }
+    document.write('</br>');
+  }
+};
